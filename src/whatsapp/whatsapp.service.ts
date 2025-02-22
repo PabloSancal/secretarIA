28ae88a93b2ea095bfb79a4a47bad39162d69ec6
Aspasia1337 <thereald3al@proton.me>
2025-02-22 06:39:17 +0100

    Username feature added & fixes

--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -8,37 +8,42 @@ import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class WhatsappService implements OnModuleInit {
-  private client: Client;
-  private readonly logger = new Logger(WhatsappService.name);
+  private client: Client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+      product: 'chrome',
+      executablePath: '/usr/bin/chromium-browser',
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless'],
+    },  
+  });
 
+    private readonly logger = new Logger(WhatsappService.name);
+
+
+  
   constructor(
     private eventEmitter: EventEmitter2,
     private configService: ConfigService,
     private messagesService: MessagesService,
     private iaModelService: IaModelService,
     private userService: UsersService,
-  ) {
-    this.client = new Client({
-      authStrategy: new LocalAuth(),
-      puppeteer: {
-        product: "chrome",
-        executablePath: "/usr/bin/chromium-browser",
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless']
-      }
-    });
-  }
+    ) {};
 
-  onModuleInit() {
-    this.client.on('qr', (qr) => {
-      const port = this.configService.get<number>('PORT');
-      const apiPath = this.configService.get<string>('API_URL');
-      this.logger.log(`QrCode: ${apiPath}:${port}/whatsapp/qrcode`);
-      this.eventEmitter.emit('qrcode.created', qr);
-    });
+  
 
-    this.client.on('ready', () => {
-      this.logger.log('Conexión exitosa !!');
-    });
+    onModuleInit() {
+        this.client.on('qr', (qr) => {
+            const port = this.configService.get<number>('PORT')
+            const apiPath = this.configService.get<string>('API_URL')
+            this.logger.log(
+                `QrCode: ${apiPath}:${port}/whatsapp/qrcode`,
+            );
+            this.eventEmitter.emit('qrcode.created', qr);
+        });
+
+        this.client.on('ready', () => {
+            this.logger.log("Conexión exitosa !!");
+        });
 
         this.client.on('message', async (msg) => {
             this.logger.verbose(`${msg.from}: ${msg.body}`);
@@ -52,10 +57,12 @@ export class WhatsappService implements OnModuleInit {
                 const command = msg.body.match(/^!(\S*)/);
             if (command && msg.body.charAt(0) === '!') {
                 if (command[0] === '!name') {
-                    //TODO: actualizar name de user
+                    
 
-                } else if (command[0] === '!hi') {
-                    msg.reply('Buenas y santas');
+                } else if (command[0] === '!username') {
+                    const message = msg.body.slice(command[0].length + 1)
+                    const userName = await this.userService.changeName(message,phoneNumber)
+                    return msg.reply(userName.name)
 
                 } else if (command[0] === '!message') {
                     const message = msg.body.slice(command[0].length + 1)
