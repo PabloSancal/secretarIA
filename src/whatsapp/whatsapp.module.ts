f4fade0091ff86bdb2367249f8f642e9b6b9fb2a
Pavlo <pscalero15@gmail.com>
2025-02-23 01:47:32 +0100

    Recordatorios done

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
 import { WhatsappController } from './whatsapp.controller';
 import { MessagesModule } from 'src/messages/messages.module';
 import { IaModelModule } from 'src/ia-model/ia-model.module';
 import { UsersService } from 'src/users/users.service';
+import { RecordatoriosModule } from 'src/recordatorios/recordatorios.module';
 
 @Module({
-  imports: [MessagesModule, IaModelModule],
+  imports: [
+    MessagesModule, 
+    IaModelModule,
+    forwardRef(() => RecordatoriosModule), 
+    ],
   controllers: [WhatsappController],
   providers: [
     WhatsappService,
